d60bfe611327c45c39299e050d077599c7ca77c8
Bui Anh Tu <buianhtu3004@gmail.com>
2017-12-08 14:39:38 +0700

    intro_network

--- assets/Script/IntroScripts/Intro.js
+++ assets/Script/IntroScripts/Intro.js
@@ -1,3 +1,6 @@
+var MyMessage = require('initialize_pb');
+var NetworkManager = require('NetworkManager');
+
 cc.Class({
     extends: cc.Component,
 
@@ -47,8 +50,90 @@ cc.Class({
 
     },
     goGame: function() {
-        cc.director.loadScene('Lobby');
+        window.ws = new WebSocket("ws://192.168.0.32:1280/megajackpot");
+        window.listMessage = [];
+        window.ws.binaryType = "arraybuffer";
+
+        window.ws.onopen = function (event) {
+            console.log("Web socket");
+            setTimeout(function() {
+                var message = new MyMessage.BINInitializeRequest();
+                message.setCp("18");
+                message.setAppversion("19");
+                message.setDeviceid("00000000");
+                message.setDeviceinfo("NO_DEVICE");
+                message.setCountry("vn");
+                message.setLanguage("vi");
+                message.setPakagename("com.daigia777.gamemon");
+                var data = NetworkManager.initData(message.serializeBinary(), NetworkManager.OS.ANDROID,
+                    NetworkManager.MESSAGE_ID.INITIALIZE, "");
+                cc.log("data:", data);
+                window.ws.send(data);
+            }, 1);
+        };
+        window.ws.onclose = function (event) {
+            console.log("Websocket instance was closed");
+        };
+        window.ws.onmessage = this.ongamestatus.bind(this);
+
+        setTimeout(function () {
+            if(window.ws.readyState == WebSocket.OPEN) {
+                cc.log("web socket is open");
+            } else {
+                console.log("Web socket instance wasn't ready");
+            }
+        }, 3);
 
         this.unschedule(this.goGame);
+    },
+    ongamestatus: function(event) {
+        cc.log("response text msg:" + event);
+        if(event.data!==null || event.data !== 'undefined') {
+            var lstMessage = NetworkManager.parseFrom(event.data, event.data.byteLength);
+            cc.log("list message size:" + lstMessage.length);
+            if(lstMessage.length > 0) {
+                var buffer = lstMessage.shift();
+                cc.log("buffer:" , buffer);
+                this.handleMessage(buffer);
+            }
+        }
+    },
+    handleMessage: function(buffer) {
+        switch (buffer.message_id) {
+            case NetworkManager.MESSAGE_ID.INITIALIZE:
+                var msg = buffer.response;
+                cc.log("message: " + msg.getMessage());
+                this.initialMessageResponseHandler(msg);
+                //cc.director.loadScene('Lobby');
+                break;
+        }
+    },
+    initialMessageResponseHandler: function(initialMessage) {
+        cc.log("initialMessage", initialMessage);
+        if (initialMessage != 0) {
+            //common.initialize = initialMessage.responseCode;
+            if (initialMessage.getResponsecode()) {
+                var serverAppVersion = initialMessage.getCurrentappversion();
+                cc.log("serverAppVersion = ", serverAppVersion);
+                var hot_lines = [];
+                cc.log("hot_line size", initialMessage.getHotlinesList().length);
+                for (var i = 0; i < initialMessage.getHotlinesList().length; i++){
+                    hot_lines.push(initialMessage.getHotlinesList()[i]);
+                }
+
+                /*Set enable game ids*/
+                var _gameIds = [];
+                for (var i = 0; i < initialMessage.getEnablegameidsList().length; i++) {
+                    _gameIds.push(initialMessage.getEnablegameidsList()[i]);
+                }
+
+                cc.log("game id = ", _gameIds);
+                cc.director.loadScene('Lobby');
+
+            }else {
+                // PopupMessageBox* popupMessage = new PopupMessageBox();
+                // popupMessage.showPopup(init_response.message());
+            }
+        }
     }
 });
