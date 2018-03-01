7d3e1681371b3c19acffdbcad0cefc985f47f797
HungLe <hungle@HungLes-iMac.local>
2018-03-01 18:20:47 +0700

    add node napthe

---
+++ assets/script/popup/node/NodeNapThe.js
@@ -0,0 +1,20 @@
+
+cc.Class({
+    extends: cc.Component,
+
+    properties: {
+        edit_number : cc.EditBox,
+        edit_serial : cc.EditBox,
+        table_view : cc.Node,
+        ui_left : cc.Node,
+
+    },
+
+    onLoad: function () {
+
+    },
+
+    start: function () {
+
+    },
+});
