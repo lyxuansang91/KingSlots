4732f6fe514bd0c457a5c66e75450a457d3b6547
Bui Anh Tu <buianhtu3004@gmail.com>
2018-03-21 09:40:41 +0700

    tuatula_add_taixiu_statis

---
+++ assets/Script/Popup/PopupTaixiuStatis.js
@@ -0,0 +1,15 @@
+var NetworkManager = require('NetworkManager');
+var Popup = require('Popup');
+cc.Class({
+    extends: Popup,
+
+    properties: {
+        contentBottom: cc.Node
+    },
+
+    init: function (session) {
+
+    },
+
+
+});
