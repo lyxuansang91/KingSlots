ffcb88c3ef0e4310215ce319d0bdb5623361b17b
anhtu300488 <buianhtu3004@gmail.com>
2018-04-20 16:11:44 +0700

    tuatula_popup_setting

--- assets/Script/Scene/Login/Login.js
+++ assets/Script/Scene/Login/Login.js
@@ -11,6 +11,12 @@ cc.Class({
         messagePopup: cc.Prefab,
         bar_top_login : cc.Node,
         bar_top_lobby : cc.Node,
+        popup_setting : cc.Node,
+        list_frame: [cc.SpriteFrame],
+        musicBtn: cc.Button,
+        soundBtn: cc.Button,
+        vibrateBtn: cc.Button,
+        isSetting: false
     },
     // use this for initialization
     onLoad: function () {
@@ -26,6 +32,28 @@ cc.Class({
             this.bar_top_login.active = true;
             this.bar_top_lobby.active = false;
         }
+
+        var musicStatus = Common.getPrefs(Config.prefKey.MUSIC);
+        var soundStatus = Common.getPrefs(Config.prefKey.SOUND);
+        var vibrateStatus = Common.getPrefs(Config.prefKey.VIBARTE);
+        if(musicStatus === "true"){
+            this.musicBtn.getComponent(cc.Sprite).spriteFrame = this.list_frame[1];
+        } else {
+            this.musicBtn.getComponent(cc.Sprite).spriteFrame = this.list_frame[0];
+        }
+
+        if(soundStatus === "true"){
+            this.soundBtn.getComponent(cc.Sprite).spriteFrame = this.list_frame[1];
+        } else {
+            this.soundBtn.getComponent(cc.Sprite).spriteFrame = this.list_frame[0];
+        }
+
+        if(vibrateStatus === "true"){
+            this.vibrateBtn.getComponent(cc.Sprite).spriteFrame = this.list_frame[1];
+        } else {
+            this.vibrateBtn.getComponent(cc.Sprite).spriteFrame = this.list_frame[0];
+        }
+
     },
 
     start : function () {
@@ -402,6 +430,62 @@ cc.Class({
         if (userSetting.hasAutodenyinvitation()) {
             Common.setAutoDenyInvitation(userSetting.getAutodenyinvitation());
             cc.sys.localStorage.setItem("DENY_INVITES", userSetting.getAutodenyinvitation());
+        }
+    },
+    settingClick: function () {
+        if(this.isSetting === false){
+            this.popup_setting.active = true;
+        } else {
+            this.popup_setting.active = false;
+        }
+        this.isSetting = !this.isSetting;
+    },
+    connectFacebook: function () {
+        Common.openIdConnectRequest(Common.FACEBOOK_CHANNEL);
+    },
+
+    onClickExit: function(){
+
+        Common.showPopup(Config.name.POPUP_MESSAGE_BOX,function(message_box) {
+            message_box.init(Common.KEYTEXT.MSG_LOG_OUT, Config.COMMON_POPUP_TYPE.MESSAGE_BOX.MESSAGEBOX_TYPE, function() {
+                NetworkManager.getLogoutMessageFromServer(true);
+            });
+            message_box.appear();
+        });
+    },
+
+    musicClick: function () {
+        this.changeOnOffSetting(Config.prefKey.MUSIC);
+    },
+
+    soundClick: function () {
+        this.changeOnOffSetting(Config.prefKey.SOUND);
+    },
+
+    vibrateClick: function () {
+        this.changeOnOffSetting(Config.prefKey.VIBARTE);
+    },
+
+    changeOnOffSetting: function(pkey) {
+        if (Common.getPrefs(pkey) === 'true'){
+            Common.setPrefs(pkey, false);
+            if(pkey === Config.prefKey.MUSIC){
+                this.musicBtn.getComponent(cc.Sprite).spriteFrame = this.list_frame[0];
+            } else if(pkey === Config.prefKey.SOUND){
+                this.soundBtn.getComponent(cc.Sprite).spriteFrame = this.list_frame[0];
+            } else if(pkey === Config.prefKey.VIBARTE){
+                this.vibrateBtn.getComponent(cc.Sprite).spriteFrame = this.list_frame[0];
+            }
+        }
+        else {
+            Common.setPrefs(pkey, true);
+            if(pkey === Config.prefKey.MUSIC){
+                this.musicBtn.getComponent(cc.Sprite).spriteFrame = this.list_frame[1];
+            } else if(pkey === Config.prefKey.SOUND){
+                this.soundBtn.getComponent(cc.Sprite).spriteFrame = this.list_frame[1];
+            } else if(pkey === Config.prefKey.VIBARTE){
+                this.vibrateBtn.getComponent(cc.Sprite).spriteFrame = this.list_frame[1];
+            }
         }
     }
 });
